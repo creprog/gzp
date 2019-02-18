f2f06e34f6850c1e88a15c8256de8788caadccbb
creprog <minercre@gmail.com>
2019-02-18 19:13:13 +0300

    Update tpa.py

--- tpa.py
+++ tpa.py
@@ -15,7 +15,7 @@ def saveData(grsName, grsData):
                         cell.text = grsData[i]
                         cell.paragraphs[0].alignment = currentAligment
                         cell.paragraphs[0].runs[0].font.name = currentFont
-    document.save(grsName + ', ' + grsData[6] + '.docx')
+    document.save(grsData[6] + '.docx')
 
 def useData(first_line, last_line):
     wb = load_workbook('tpa.xlsx')
@@ -23,38 +23,45 @@ def useData(first_line, last_line):
 
     # i = current line
     for i in range(first_line, last_line+1):
-        # prepairig datalist for form
-        wfc = []
-        # name of GRS
-        d = ws['A' + str(i)]
-        current_grs = d.value        
-        # type of tpa
-        d = ws['F' + str(i)]
-        wfc.append(d.value)
-        # marka tpa
-        d = ws['G' + str(i)]
-        wfc.append(d.value)
-        # Dy tpa
-        d = ws['H' + str(i)]
-        wfc.append(str(d.value))
-        # Py tpa
-        d = ws['I' + str(i)]
-        currentText = str(d.value / 10)
-        currentText = currentText.replace('.', ',')
-        wfc.append(currentText)
-        # Working gas    
-        wfc.append('Природный газ')
-        # Date of build
-        d = ws['J' + str(i)]
-        dt = d.value
-        d = dt.strftime("%d.%m.%Y")
-        wfc.append(d)
-        # Place and number
-        d = ws['C' + str(i)]
-        d1 = ws['D' + str(i)]
-        wfc.append(str(d.value + ', №' + d1.value))               
-        saveData(current_grs, wfc)
-        print(str(i - first_line + 1) + '/' + str(last_line - first_line + 1))
+        try:
+            # prepairig datalist for form
+            wfc = []
+            # name of GRS
+            d = ws['A' + str(i)]
+            current_grs = d.value        
+            # type of tpa
+            d = ws['F' + str(i)]
+            wfc.append(d.value)
+            # marka tpa
+            try:
+                d = ws['G' + str(i)]
+                wfc.append(d.value)
+            except:
+                wfc.append(' ')
+            # Dy tpa
+            d = ws['H' + str(i)]
+            wfc.append(str(d.value))
+            # Py tpa
+            d = ws['I' + str(i)]
+            currentText = str(d.value / 10)
+            currentText = currentText.replace('.', ',')
+            wfc.append(currentText)
+            # Working gas    
+            wfc.append('Природный газ')
+            # Date of build
+            d = ws['J' + str(i)]
+            dt = d.value
+            d = dt.strftime("%d.%m.%Y")
+            wfc.append(d)
+            # Place and number
+            d = ws['C' + str(i)]
+            d1 = ws['D' + str(i)]
+            full_line = 'ГРС ' + current_grs + ', ' + str(d.value).capitalize() + ', №' + str(d1.value)
+            wfc.append(full_line)
+            saveData(current_grs, wfc)
+            print(str(i) + '/' + str(last_line))
+        except:
+            print('Не сделан паспорт для строки: ' + str(i))
     
 fLine = input("Введите номер начальной строки таблицы: ")
 lLine = input("Введите номер последней строки таблицы: ")
